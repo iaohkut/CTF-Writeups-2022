6493862903e0f048aee5e5cf677e459f9d83a3f6
Nguyễn Công Tú <77691959+iaohkut@users.noreply.github.com>
2022-06-30 19:17:32 +0700

    Update enc.py

--- FPTHackingCTF/RRSSAA/enc.py
+++ FPTHackingCTF/RRSSAA/enc.py
@@ -14,21 +14,12 @@ n1 = p1 * q1 * r1 * s1
 msg1 = bytes_to_long(c1)
 ct1 = pow(msg1, e, n1)
 
-
-
-
-
-
-
-
-
 p2 = getPrime(1024)
 q2 = nextprime(p2)
 n2 = p2 * q2
 
 msg2 = bytes_to_long(c2)
 ct2 = pow(msg2, e, n2)
-
 
 print(f'e = {e}')
 print(f'n1 = {n1}')
